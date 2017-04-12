a35fb57b0a484268f35c5ed1dd098897cac65eea
jiaconis <john.t.iaconis@gmail.com>
2017-04-12 13:23:48 -0700

    Added state to GroceryListItem component

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+// var food = ['Chocolate', 'Peanutbutter', 'Oreos'];
+
+class GroceryListItem extends React.Component {
+  constructor(props) { 
+    super(props);
+
+    this.state = {
+      hover: false
+    }
+  }
+
+  onListItemMouseHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render () {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'none'
+    };
+
+    return (
+    <li style={style} onMouseEnter={this.onListItemMouseHover.bind(this)} >{this.props.item}</li>
+    );
+
+  }
+
+
+
+
+}
+
+var GroceryList = (props) => (
+  <ul>
+  {props.items.map( item => 
+    <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+
+ReactDOM.render(<GroceryList items = {['Chocolate', 'Peanutbutter', 'Oreos']}/>, document.getElementById('app'));
+
+
+
